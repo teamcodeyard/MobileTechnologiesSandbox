2a9bf3bdcd23063f152ef7dd6479b37e081bec88
csellelajos <cselle.lajos@codeyard.eu>
2019-11-05 10:44:40 +0100

    android annotations config added

--- app/src/main/java/eu/codeyard/sandbox/MainActivity.java
+++ app/src/main/java/eu/codeyard/sandbox/MainActivity.java
@@ -4,11 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 }
